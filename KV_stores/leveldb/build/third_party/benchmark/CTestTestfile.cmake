b1714c357b33e5460ef9cd3ae458cb42fb4c2f5b
CODER-UCAS <81192wzz@gmail.com>
2024-02-29 00:27:12 +0800

    sync

--- KV_stores/leveldb/build/third_party/benchmark/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangzizhao/WorkloadAnalysis/KV_stores/leveldb/third_party/benchmark
-# Build directory: /home/wangzizhao/WorkloadAnalysis/KV_stores/leveldb/build/third_party/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- KV_stores/leveldb/build/third_party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangzizhao/WorkloadAnalysis/KV_stores/leveldb/third_party/googletest/googletest
-# Build directory: /home/wangzizhao/WorkloadAnalysis/KV_stores/leveldb/build/third_party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
