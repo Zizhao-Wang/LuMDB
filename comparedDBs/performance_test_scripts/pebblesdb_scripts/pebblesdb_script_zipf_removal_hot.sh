29c7a715fbf7ca8d1d2faf779dcaaea3b19855d2
CODER-UCAS <81192wzz@gmail.com>
2024-06-06 14:43:15 +0800

    sync

--- comparedDBs/performance_test_scripts/pebblesdb_scripts/pebblesdb_script_zipf_removal_hot.sh
+++ comparedDBs/performance_test_scripts/pebblesdb_scripts/pebblesdb_script_zipf_removal_hot.sh
@@ -7,7 +7,6 @@ sudo bash -c 'ulimit -n 800000'
 
 BASE_VALUE_SIZE=128
 billion=1000000000
-percentages=(1 5 10 15 20 25 30) # 定义百分比值
 range_dividers=(1)
 DEVICE_NAME="nvme1n1"
 
@@ -41,108 +40,84 @@ for i in {10..10}; do
 
             num_format=$(convert_to_billion_format $num_entries)
 
-            for zipf_a in 1.1 1.2 1.3 1.4 1.5; do  # 1.2 1.3 1.4 1.5
-                    percentages1=() # 1 5 10 15 20 25 30
-                    No_hot_percentages=(0 10 20 30 40 50 60 70 80 90 100)
+            for zipf_a in 1.2 1.3 1.4 1.5; do  #1.1  1.2 1.3 1.4 1.5
 
-                    for no_hot in "${No_hot_percentages[@]}"; do
+                # log_file="leveldb2_${num_format}_val_${value_size}_zipf${zipf_a}_1-30.log"
+                log_file="Pebblesdb_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}.log"
+                data_file="/home/jeff-wang/workloads/zipf${zipf_a}_keys10.0B.csv" # 构建数据文件路径
 
-                        # log_file="leveldb2_${num_format}_val_${value_size}_zipf${zipf_a}_1-30.log"
-                        log_file="Pebblesdb_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}.log"
-                        data_file="/home/jeff-wang/workloads/zipf${zipf_a}_keys10.0B.csv" # 构建数据文件路径
-                        # hot_files=$(printf "/home/jeff-wang/workloads/zipf${zipf_a}_top%%s_keys10B.csv," {1,5,10,15,20,25,30})
-                        # hot_files=${hot_files%?} # 移除最后一个逗号
+                # 如果日志文件存在，则跳过当前迭代
+                if [ -f "$log_file" ]; then
+                    echo "Log file $log_file already exists. Skipping this iteration."
+                    continue
+                fi
 
-                        
-                        hot_files=""
-                        for percent in "${percentages1[@]}"; do
-                            if [[ -z "$hot_files" ]]; then
-                                # 第一次迭代时，直接赋值
-                                hot_files="/home/jeff-wang/workloads/zipf${zipf_a}_top${percent}_keys10.0B.csv"
-                            else
-                                # 后续迭代时，在现有字符串后面添加
-                                hot_files="$hot_files,/home/jeff-wang/workloads/zipf${zipf_a}_top${percent}_keys10.0B.csv"
-                            fi
-                        done
+                echo "base_num: $base_num"
+                echo "num_entries: $num_entries"
+                echo "value_size:$value_size"
+                echo "stats_interval: $stats_interva"
+                echo "$num_format"
 
-                        echo "hot_files: $hot_files"
-                        percentages_str="" #,5,10,15,20,25,30
-                        for percent in "${percentages1[@]}"; do
-                            if [[ -z "$percentages_str" ]]; then
-                                # 第一次迭代时，直接赋值
-                                percentages_str="${percent}"
-                            else
-                                # 后续迭代时，在现有字符串后面添加
-                                percentages_str="$percentages_str,${percent}"
-                            fi
-                        done
-
-                        # 如果日志文件存在，则跳过当前迭代
-                        if [ -f "$log_file" ]; then
-                            echo "Log file $log_file already exists. Skipping this iteration."
-                            continue
-                        fi
-
-                        echo "base_num: $base_num"
-                        echo "num_entries: $num_entries"
-                        echo "value_size:$value_size"
-                        echo "stats_interval: $stats_interva"
-                        echo "$num_format"
-
-                        iostat -d 100 -x $DEVICE_NAME > Pebblesdb_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}_IOstats.log &
-                        PID_IOSTAT=$!
+                iostat -d 100 -x $DEVICE_NAME > Pebblesdb_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}_IOstats.log &
+                PID_IOSTAT=$!
                     
-                        ../../../pebblesdb/release/db_bench \
-                        --db=/mnt/nvm/level8B \
-                        --num=$num_entries \
-                        --value_size=$value_size \
-                        --batch_size=1000 \
-                        --benchmarks=fillzipf,stats \
-                        --hot_file=$hot_files \
-                        --data_file=$data_file  \
-                        --percentages=$percentages_str \
-                        --logpath=/mnt/logs \
-                        --bloom_bits=10 \
-                        --log=1  \
-                        --cache_size=8388608 \
-                        --No_hot_percentage=$no_hot \
-                        --open_files=40000 \
-                        --compression=0 \
-                        --stats_interval=$stats_interva \
-                        --histogram=1 \
-                        --write_buffer_size=1048576 \
-                        --max_file_size=2097152   \
-                        --print_wa=true \
-                        &> >( tee $log_file) &  
+                ../../../pebblesdb/release/db_bench \
+                --db=/mnt/nvm/pebbles10B \
+                --num=$num_entries \
+                --value_size=$value_size \
+                --batch_size=1000 \
+                --benchmarks=filletc,stats \
+                --data_file=$data_file  \
+                --logpath=/mnt/logs \
+                --bloom_bits=10 \
+                --log=1  \
+                --cache_size=8388608 \
+                --open_files=40000 \
+                --compression=0 \
+                --stats_interval=$stats_interva \
+                --histogram=1 \
+                --write_buffer_size=1048576 \
+                --max_file_size=2097152   \
+                --print_wa=true \
 
-                        # 保存 db_bench 的 PID 供监控使用
-                        sleep 1
+                --num=$num_entries \
+                --benchmarks=filletc,stats \
+        --bloom_bits=10 \
+        --cache_size=8388608  \
+        --open_files=40000  \
+        --histogram=1 \
+        --print_wa=true \
+        --stats_interval=$stats_interval \
+        --write_buffer_size=67108864  \
+        --max_file_size=67108864 \
+        --data_file=$data_file \
+                &> >( tee $log_file) &  
 
-                        DB_BENCH_PID=$(pgrep -af 'db_bench --db=/mnt/nvm/level8B' | grep -v 'sudo' | awk '{print $1}')
-                        echo "Selected DB_BENCH_PID: $DB_BENCH_PID"
+                # 保存 db_bench 的 PID 供监控使用
+                sleep 1
 
-                        perf stat -p $DB_BENCH_PID 2>&1 | tee "perf_stat_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}.txt" &
-                        PERF_PID=$!
+                DB_BENCH_PID=$(pgrep -af 'db_bench --db=/mnt/nvm/pebbles10B' | grep -v 'sudo' | awk '{print $1}')
+                echo "Selected DB_BENCH_PID: $DB_BENCH_PID"
 
-                        wait $DB_BENCH_PID
+                perf stat -p $DB_BENCH_PID 2>&1 | tee "perf_stat_${num_format}_val_${value_size}_zipf${zipf_a}_Nohot1-${no_hot}.txt" &
+                PERF_PID=$!
 
-                        # 结束 iostat 进程
-                        echo "Checking if iostat process with PID $PID_IOSTAT is still running..."
-                        ps -p $PID_IOSTAT
-                        if [ $? -eq 0 ]; then
-                            echo "iostat process $PID_IOSTAT is still running, killing now..."
-                            kill -9 $PID_IOSTAT
-                            if [ $? -eq 0 ]; then
-                                echo "iostat process $PID_IOSTAT has been successfully killed."
-                            else
-                                echo "Failed to kill iostat process $PID_IOSTAT."
-                            fi
-                        else
-                            echo "iostat process $PID_IOSTAT is no longer running."
+                wait $DB_BENCH_PID
+
+                # 结束 iostat 进程
+                echo "Checking if iostat process with PID $PID_IOSTAT is still running..."
+                ps -p $PID_IOSTAT
+                if [ $? -eq 0 ]; then
+                    echo "iostat process $PID_IOSTAT is still running, killing now..."
+                    kill -9 $PID_IOSTAT
+                    if [ $? -eq 0 ]; then
+                            echo "iostat process $PID_IOSTAT has been successfully killed."
+                    else
+                            echo "Failed to kill iostat process $PID_IOSTAT."
                         fi
-
-
-                    done
+                    else
+                    echo "iostat process $PID_IOSTAT is no longer running."
+                fi
             done
         done
 done
