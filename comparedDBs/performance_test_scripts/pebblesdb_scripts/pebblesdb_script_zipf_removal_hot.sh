06ee7a890c3ba84e7d4defe596bf9b2f385a7f88
CODER-UCAS <81192wzz@gmail.com>
2024-10-14 17:51:28 +0800

    sync

--- comparedDBs/performance_test_scripts/pebblesdb_scripts/pebblesdb_script_zipf_removal_hot.sh
+++ comparedDBs/performance_test_scripts/pebblesdb_scripts/pebblesdb_script_zipf_removal_hot.sh
@@ -37,12 +37,12 @@ for i in {10..10}; do
 
             num_format=$(convert_to_billion_format $num_entries)
 
-            for zipf_a in 1.3; do  #1.1  1.2 1.3 1.4 1.5
+            for zipf_a in 1.1; do  #1.1  1.2 1.3 1.4 1.5
 
                 # log_file="leveldb2_${num_format}_val_${value_size}_zipf${zipf_a}_1-30.log"
-                log_file="Pebbles10B_${num_format}_val_${value_size}_mem1MB_zipf${zipf_a}.log"
+                log_file="Pebbles10B_${num_format}_val_${value_size}_mem8MB_zipf${zipf_a}.log"
                 data_file="/home/jeff-wang/workloads/zipf${zipf_a}_keys10.0B.csv" # 构建数据文件路径
-                memory_log_file="/home/jeff-wang/WorkloadAnalysis/comparedDBs/performance_test_scripts/pebblesdb_scripts/10B_Pebblesdb_zipf_hot_removal/Pebblesdb_memory_usage_${num_format}_key16_val${value_size}_zipf${zipf_a}.log"
+                memory_log_file="/home/jeff-wang/WorkloadAnalysis/comparedDBs/performance_test_scripts/pebblesdb_scripts/10B_Pebblesdb_zipf_hot_removal/Pebblesdb_memory_8MiB_${num_format}_key16_val${value_size}_zipf${zipf_a}.log"
 
                 # 如果日志文件存在，则跳过当前迭代
                 if [ -f "$log_file" ]; then
@@ -86,8 +86,8 @@ for i in {10..10}; do
                 --compression=0 \
                 --stats_interval=$stats_interva \
                 --histogram=1 \
-                --write_buffer_size=1048576 \
-                --max_file_size=1048576   \
+                --write_buffer_size=8388608 \
+                --max_file_size=8388608   \
                 --print_wa=true \
                 &> >( tee $log_file) &  
 
